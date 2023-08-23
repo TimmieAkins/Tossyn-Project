6dc853f16244cc1d97070f0b5901e89fd4499a85
TimmieAkins <timmieakinde@gmail.com>
2023-08-23 15:47:29 +0100

    added script setup to AboutViewComp

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,11 @@
-<script>
+<script setup>
 import { defineComponent } from 'vue';
 
 import AboutIntro from '../components/AboutSection/AboutIntro.vue';
 import MyStory from '../components/AboutSection/MyStory.vue';
-import FooterSection from '../components/HomeSection/FooterSection.vue';
+import FooterSection from './../components/HomeSection/FooterSection.vue';
 
-export default defineComponent({
-    components: {
-        AboutIntro,
-        MyStory,
-        FooterSection
-    }
-})
+
 </script>
 
 <template>
